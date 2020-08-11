2d71584a275138726a7860a0ce9b92b1b3b27abb
MarcinSchmechel <marcin.schmechel@o2.pl>
2020-08-11 20:54:43 +0200

    Zadanie 29.1 - dodane testy

--- src/test/java/com/crud/tasks/mapper/MapperTestSuite.java
+++ src/test/java/com/crud/tasks/mapper/MapperTestSuite.java
@@ -1,9 +1,6 @@
 package com.crud.tasks.mapper;
 
-import com.crud.tasks.domain.TrelloBoard;
-import com.crud.tasks.domain.TrelloBoardDto;
-import com.crud.tasks.domain.TrelloList;
-import com.crud.tasks.domain.TrelloListDto;
+import com.crud.tasks.domain.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +19,9 @@ public class MapperTestSuite {
     @Autowired
     private TrelloMapper trelloMapper;
 
+    @Autowired
+    private TaskMapper taskMapper;
+
     @Test
     public void mapToBoardsTest() {
     //Given
@@ -37,7 +37,9 @@ public class MapperTestSuite {
         assertNotNull(mappedTrelloBoard);
         assertEquals(1,mappedTrelloBoard.size());
         assertTrue(mappedTrelloBoard.get(0).getName().contains("Board1"));
+        assertTrue(mappedTrelloBoard.get(0).getLists().get(0).getName().contains("List1"));
     }
+
     @Test
     public void mapToBoardsDtoTest() {
         //Given
@@ -53,5 +55,49 @@ public class MapperTestSuite {
         assertNotNull(mappedTrelloBoardDto);
         assertEquals(1,mappedTrelloBoardDto.size());
         assertTrue(mappedTrelloBoardDto.get(0).getName().contains("Board1"));
+        assertTrue(mappedTrelloBoardDto.get(0).getLists().get(0).getName().contains("List1"));
+    }
+
+    @Test
+    public void mapToTaskTest() {
+        //Given
+        TaskDto taskDto = new TaskDto(1L, "Test1", "Test_content");
+
+        //When
+        Task task = taskMapper.mapToTask(taskDto);
+
+        //Then
+        assertNotNull(task);
+        assertTrue(task.getTitle().contains("Test1"));
+    }
+
+    @Test
+    public void mapToTaskDtoTest() {
+        //Given
+        Task task = new Task(1L, "Test1", "Test_content");
+
+        //When
+        TaskDto taskDto = taskMapper.mapToTaskDto(task);
+
+        //Then
+        assertNotNull(taskDto);
+        assertTrue(taskDto.getTitle().contains("Test1"));
+    }
+
+    @Test
+    public void mapToTaskDtoListTest() {
+        //Given
+        List<Task> taskList = new ArrayList<>();
+        taskList.add(new Task(1L, "Test1", "Test_content1"));
+        taskList.add(new Task(2L, "Test2", "Test_content2"));
+        taskList.add(new Task(3L, "Test3", "Test_content3"));
+
+        //When
+        List<TaskDto> taskDtoList= taskMapper.mapToTaskDtoList(taskList);
+
+        //Then
+        assertNotNull(taskDtoList);
+        assertEquals(3,taskDtoList.size());
+        assertTrue(taskDtoList.get(2).getTitle().contains("Test3"));
     }
 }
